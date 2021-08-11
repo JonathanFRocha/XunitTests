0425671451f00e7155f4b1f59478c7326d5201f0
JonathanFRocha <jonathanferreira.contato@gmail.com>
2021-08-11 15:40:31 -0300

    Customer tests finished

---
+++ calculations/calculation.tests/CustomerTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace calculations.tests
+{
+    public class CustomerTests
+    {
+        [Fact]
+        public void NameNotEmpty()
+        {
+            var customer = new Customer();
+            Assert.False(string.IsNullOrEmpty(customer.Name));
+        }
+
+        [Fact]
+        public void checkLegitimateForDiscount()
+        {
+            var customer = new Customer();
+            Assert.InRange(customer.Age, 25, 40);
+        }
+    }
+}
---
+++ calculations/calculations/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculations
+{
+    public class Customer
+    {
+        public string Name = "Jonathan";
+        public int Age = 35;
+    }
+}
